9b501b857cad090d03591a857de738efa87fc699
Mattlu88 <24664073+Mattlu88@users.noreply.github.com>
2019-07-15 17:11:36 +1000

    Working on Bin2Dec project

--- projects/borderRadiusPreviewer/dom.js
+++ projects/borderRadiusPreviewer/dom.js
@@ -1,17 +1,36 @@
 
-var radiusInputs = [...document.getElementsByClassName("radius-input")];
+const borderDiv = () => document.getElementById("border-div");
+const textarea = () => document.getElementById("display-text");
 
-radiusInputs.forEach((rInput) => {
-    const borderDiv = document.getElementById("border-div");
+const radiusInputs = () => [...document.getElementsByClassName("radius-input")];
+radiusInputs().forEach((rInput) => {
     const initRaidus = "11";
     rInput.addEventListener("input", ()=> {
-        setBorderRadius(borderDiv, rInput);
+        setBorderRadius(borderDiv(), rInput);
     });
     rInput.value = initRaidus;
-    setBorderRadius(borderDiv, rInput);
+    setBorderRadius(borderDiv(), rInput);
 });
 
+const browserTypes = () => [...document.getElementsByClassName("browser-type")];
+browserTypes().forEach((browserType) => {
+   browserType.addEventListener("input", ()=> {
+       setTextarea(borderDiv());
+   }); 
+});
+
+const copyBtn = () => document.getElementById("copy-btn");
+copyBtn().addEventListener("click", () => {copyTextarea()});
+
 function setBorderRadius(div, elmnt) {
+    if (elmnt.value.length > 3) {
+        elmnt.classList.remove("w-25");
+        elmnt.classList.add("w-50");
+    } else {
+        elmnt.classList.remove("w-50");
+        elmnt.classList.add("w-25");
+    };
+
     switch (elmnt.id) {
         case "top-left-input":
             div.style.borderTopLeftRadius = elmnt.value + 'px';
@@ -28,24 +47,31 @@ function setBorderRadius(div, elmnt) {
 
     };
 
-    const textarea = document.getElementById("display-text");
-    setTextarea(div, textarea);
+    setTextarea(div);
+};
 
-    function setTextarea(div, textarea) {
-        const browserTypes = [...document.getElementsByClassName("browser-type")];
-        browserTypes.forEach((type) => {
-            switch (type.id) {
+function setTextarea(div) {
+    textarea().innerHTML = "";
+    const browserTypes = () => [...document.getElementsByClassName("browser-type")];
+    browserTypes().forEach((type) => {
+        if (type.checked) {
+            switch (type.value) {
                 case "webkit":
-                    textarea.innerHTML = "-webkit-border-radius: "
+                    textarea().innerHTML += "-webkit-border-radius: "
                     break;
                 case "gecko":
-                    textarea.innerHTML = "-moz-border-radius: "
+                    textarea().innerHTML += "-moz-border-radius: "
                     break;
                 case "css3":
-                    textarea.innerHTML = "border-radius: "
+                    textarea().innerHTML += "border-radius: "
                     break;
             };
-            textarea.innerHTML +=  div.style.borderRadius + ";";
-        });
-    };
+            textarea().innerHTML += div.style.borderRadius + ";\n";
+        };
+    });
+};
+
+function copyTextarea() {
+    textarea().select();
+    document.execCommand("copy");
 };
